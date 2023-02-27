8ecffd5e087d32b883cbcacab5c735aaa8264781
tobit bushenyula <mrbushenyula@gmail.com>
2023-02-27 10:32:04 -0800

    SimpleCalculator
    here s a simple calculator that anyone can use to calculate simple problems with the fundamental operators

---
+++ calculator.py
@@ -0,0 +1,26 @@
+from math import *
+
+num1 = int(input("num one is: "))
+
+num2 = float(input("num two is: "))
+
+#this is the new way of asking integer input from the user
+result = num1+num2
+
+print(result)
+
+#use if,elif,else to build a better calculator to push on github:
+# -first get the inputs of the two numbers and the operator from the user,then start with the if statement likethis: 
+# 
+# if op=="+":
+#  print(num1+num2)
+# elif op== "-":
+#  print(num1-num2)
+#
+# else........ 
+# etc
+
+
+########################################################################
+
+
